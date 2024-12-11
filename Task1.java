5790bc52e1f78df98fa73a97734c23db3e30b924
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-11 13:21:34 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,17 @@
+public class Task1 {
+ 
+	void disp(String name,int age) {
+
+		System.out.println(name+" "+age);
+
+	}
+
+	public static void main(String[] args) {
+
+		Task1 t=new Task1();
+
+		t.disp("jhon",15);
+
+		t.disp("sam",16);
+	}
+}
