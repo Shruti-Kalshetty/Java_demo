7ce5c563c7b484b2bcb74faf46e930e88261e3b9
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-11 13:09:58 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+ 
+	public static void main(String[] args) {
+
+		int no1=10,no2=5;
+
+		System.out.println("Addition:"+(no1+no2)+"\nSubstraction:"+(no1-no2)+
+
+				"\nMultiplication:"+(no1*no2)+"\nDivision:"+(no1/no2));
+
+	}
+ 
+}
