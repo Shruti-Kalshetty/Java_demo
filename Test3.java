1ee0875c7584a10e4442c01a1e16e90a52735143
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-11 13:11:47 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,19 @@
+public class Test3 {
+ 
+	public static void main(String[] args) {
+
+		int i=10,j=2,k=2;
+
+		System.out.println(i>j);
+
+		System.out.println(i<j);
+
+		System.out.println(k<=j);
+
+		System.out.println(i<=j && k==j);
+
+		System.out.println(i<=j || k==j);
+
+	}
+
+}
