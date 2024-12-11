54ccb43a2ee85b3188ffe01cabb26b3ecf0c5036
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-12-11 13:18:00 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"Sam", "John"};
+        int[] ages = {15, 16};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name: " + names[i] + ", Age: " + ages[i]);
+        }
+    }
+}
